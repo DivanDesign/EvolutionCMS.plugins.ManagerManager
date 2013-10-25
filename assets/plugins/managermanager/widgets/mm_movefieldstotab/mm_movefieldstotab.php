ea919030b0cd45a6f0c1707a9197224745a7b484
Ilyas Ronef <ilyas@divandesign.biz>
2013-10-25 13:35:47 +0600

    * The “mm_moveFieldsToTab” widget has been updated to 1.2.1.

--- assets/plugins/managermanager/widgets/mm_movefieldstotab/mm_movefieldstotab.php
+++ assets/plugins/managermanager/widgets/mm_movefieldstotab/mm_movefieldstotab.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * mm_moveFieldsToTab
- * @version 1.2 (2013-05-22)
+ * @version 1.2.1 (2013-10-25)
  *
  * @desc Move a field to a different tab.
  * 
- * @uses ManagerManager plugin 0.5.
+ * @uses ManagerManager plugin 0.5.2.
  * 
  * @param $fields {comma separated string} - The name(s) of the document fields (or TVs) this should apply to. @required
  * @param $tabId {string} - The ID of the tab which the fields should be moved to. Can be one of the default tab IDs or a new custom tab created with mm_createTab. @required
  * @param $roles {comma separated string} - The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles).
  * @param $templates {comma separated string} - The Id of the templates to which this widget is applied (when this parameter is empty then widget is applied to the all templates).
  * 
- * @link http://code.divandesign.biz/modx/mm_movefieldstotab/1.2
+ * @link http://code.divandesign.biz/modx/mm_movefieldstotab/1.2.1
  * 
  * @copyright 2013
  */
@@ -25,7 +25,7 @@ function mm_moveFieldsToTab($fields, $tabId, $roles = '', $templates = ''){
 	if ($e->name == 'OnDocFormRender' && useThisRule($roles, $templates)){
 		$output = "\n//  -------------- mm_moveFieldsToTab :: Begin ------------- \n";
 		
-		$output .= '$j.ddManagerManager.moveFields("'.$fields.'", "'.prepareTabId($tabId).'");';
+		$output .= '$j.ddMM.moveFields("'.$fields.'", "'.prepareTabId($tabId).'");';
 		
 		$output .= "\n//  -------------- mm_moveFieldsToTab :: End ------------- \n";
 		
