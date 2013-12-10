98e357dd3af4eebb9eb10b57bf189745c0e8063f
Ilyas Ronef <ilyas@divandesign.biz>
2013-12-10 15:52:25 +0600

    * The “mm_ddMaxLength” widget has been updated to 1.1.1.

--- assets/plugins/managermanager/widgets/ddmaxlength/ddmaxlength.php
+++ assets/plugins/managermanager/widgets/ddmaxlength/ddmaxlength.php
@@ -1,42 +1,52 @@
 <?php
 /**
  * mm_ddMaxLength
- * @version 1.1 (2013-10-02)
+ * @version 1.1.1 (2013-12-10)
  * 
  * Widget for ManagerManager plugin allowing number limitation of chars inputing in fields (or TVs).
  * 
- * @uses ManagerManager plugin 0.5.2.
+ * @uses ManagerManager plugin 0.6.
  * 
  * @param $fields {comma separated string} - The name(s) of the document fields (or TVs) which the widget is applied to. @required
  * @param $roles {comma separated string} - The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
  * @param $templates {comma separated string} - Id of the templates to which this widget is applied. Default: ''.
  * @param $length {integer} - Maximum number of inputing chars. Default: 150.
  * 
- * @link http://code.divandesign.biz/modx/mm_ddmaxlength/1.1
+ * @event OnDocFormPrerender
+ * @event OnDocFormRender
+ * 
+ * @link http://code.divandesign.biz/modx/mm_ddmaxlength/1.1.1
  * 
  * @copyright 2013, DivanDesign
  * http://www.DivanDesign.biz
  */
 
 function mm_ddMaxLength($fields = '', $roles = '', $templates = '', $length = 150){
-	global $modx, $mm_current_page, $mm_fields;
+	if (!useThisRule($roles, $templates)){return;}
+	
+	global $modx;
 	$e = &$modx->Event;
 	
-	if ($e->name == 'OnDocFormRender' && useThisRule($roles, $templates)){
-		$output = '';
+	$output = '';
+	
+	if ($e->name == 'OnDocFormPrerender'){
+		$widgetDir = $modx->config['site_url'].'assets/plugins/managermanager/widgets/ddmaxlength/';
+		
+		$output .= includeJsCss($widgetDir.'ddmaxlength.css', 'html');
+		$output .= includeJsCss($widgetDir.'jquery.ddMM.mm_ddMaxLength.js', 'html', 'jquery.ddMM.mm_ddMaxLength', '1.0');
+		
+		$e->output($output);
+	}else if ($e->name == 'OnDocFormRender'){
+		global $mm_fields;
 		
 		$fields = getTplMatchedFields($fields, 'text,textarea');
 		if ($fields == false){return;}
 		
-		$site = $modx->config['site_url'];
-		
-		$output .= "// ---------------- mm_ddMaxLength :: Begin ------------- \n";
-		//General functions
-		$output .= includeJs($site.'assets/plugins/managermanager/js/jquery.ddTools-1.8.1.min.js', 'js', 'jquery.ddTools', '1.8.1');
-		$output .= includeCss($site.'assets/plugins/managermanager/widgets/ddmaxlength/ddmaxlength.css');
+		$output .= "//---------- mm_ddMaxLength :: Begin -----\n";
 		
 		foreach ($fields as $field){
-			$output .= '
+			$output .=
+'
 $j("'.$mm_fields[$field]['fieldtype'].'[name='.$mm_fields[$field]['fieldname'].']").addClass("ddMaxLengthField").each(function(){
 	$j(this).parent().append("<div class=\"ddMaxLengthCount\"><span></span></div>");
 }).ddMaxLength({
@@ -44,35 +54,12 @@ $j("'.$mm_fields[$field]['fieldtype'].'[name='.$mm_fields[$field]['fieldname'].'
 	containerSelector: "div.ddMaxLengthCount span",
 	warningClass: "maxLengthWarning"
 });
-			';
+';
 		}
 		
-		$output .= '
-$j("#mutate").submit(function(){
-	var ddErrors = new Array();
-	$j("div.ddMaxLengthCount span").each(function(){
-		var $this = $j(this), field = $this.parents(".ddMaxLengthCount:first").parent().find(".ddMaxLengthField");
-		if (parseInt($this.text()) < 0){
-			field.addClass("maxLengthErrorField").focus(function(){
-				field.removeClass("maxLengthErrorField");
-			});
-			ddErrors.push(field.parents("tr").find("td:first-child .warning").text());
-		}
-	});
-	
-	if(ddErrors.length > 0){
-		alert("Некорректно заполнены поля: " + ddErrors.join(","));
+		$output .= "//---------- mm_ddMaxLength :: End -----\n";
 		
-		return false;
-	}else{
-		return true;
-	}
-});
-		';
-		
-		$output .= "\n// ---------------- mm_ddMaxLength :: End -------------";
-		
-		$e->output($output . "\n");
+		$e->output($output);
 	}
 }
 ?>
