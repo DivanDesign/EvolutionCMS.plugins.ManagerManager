63acd06f74a8f8f95e04d9234cf1bd2a310d1329
Segr <segr@nashalife.ru>
2014-02-26 11:06:29 +0400

    Лучше использвать db->escape, чем прямой вызов mysql_real_escape_string()

--- assets/plugins/managermanager/utilities.inc.php
+++ assets/plugins/managermanager/utilities.inc.php
@@ -209,12 +209,13 @@ function getTplMatchedFields($fields, $tvTypes = '', $tempaleId = ''){
  * @param $arr {array; comma separated string} - Values.
  */
 function makeSqlList($arr){
+	global $modx;
 	$arr = makeArray($arr);
 	
 	foreach($arr as $k => $tv){
         //if (substr($tv, 0, 2) == 'tv') {$tv=substr($tv,2);}
 		// Escape them for MySQL
-		$arr[$k] = "'".mysql_real_escape_string($tv)."'";
+		$arr[$k] = "'".$modx->db->escape($tv)."'";
 	}
 	
 	$sql = " (".implode(',', $arr).") ";
