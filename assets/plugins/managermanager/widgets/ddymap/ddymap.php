2455f2053e7d56f30bcabff560f58429fdc91873
Ilyas Ronef <ilyas@divandesign.biz>
2015-05-08 20:02:09 +0500

    * The “mm_ddYMap” widget has been updated to 1.5b.

--- assets/plugins/managermanager/widgets/ddymap/ddymap.php
+++ assets/plugins/managermanager/widgets/ddymap/ddymap.php
@@ -1,39 +1,41 @@
 <?php
 /**
  * mm_ddYMap
- * @version 1.4.3 (2013-12-10)
+ * @version 1.5b (2015-05-08)
  * 
  * @desc A widget for ManagerManager plugin allowing Yandex Maps integration.
  * 
- * @uses ManagerManager plugin 0.6.
+ * @uses ManagerManager plugin 0.6.2.
  * 
  * @param $tvs {comma separated string} - TV names to which the widget is applied. @required
  * @param $roles {comma separated string} - The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
  * @param $templates {comma separated string} - Id of the templates to which this widget is applied (when this parameter is empty then widget is applied to the all templates). Default: ''.
- * @param $w {'auto'; integer} - Width of the map container. Default: 'auto'.
- * @param $h {integer} - Height of the map container. Default: 400.
+ * @param $width {'auto'; integer} - Width of the map container. Default: 'auto'.
+ * @param $height {integer} - Height of the map container. Default: 400.
  * @param $hideField {boolean} - Original coordinates field hiding status (true — hide, false — show). Default: true.
  * 
  * @event OnDocFormPrerender
  * @event OnDocFormRender
  * 
- * @link http://code.divandesign.biz/modx/mm_ddymap/1.4.3
+ * @link http://code.divandesign.biz/modx/mm_ddymap/1.5b
  * 
- * @copyright 2013, DivanDesign
+ * @copyright 2015, DivanDesign
  * http://www.DivanDesign.biz
  */
 
-function mm_ddYMap($tvs, $roles = '', $templates = '', $w = 'auto', $h = '400', $hideField = true){
+function mm_ddYMap($tvs, $roles = '', $templates = '', $width = 'auto', $height = '400', $hideField = true){
 	if (!useThisRule($roles, $templates)){return;}
 	
 	global $modx;
 	$e = &$modx->Event;
 	
 	if ($e->name == 'OnDocFormPrerender'){
-		//The main js file including
-		$output = includeJsCss($modx->config['site_url'].'assets/plugins/managermanager/widgets/ddymap/jquery.ddMM.mm_ddYMap.js', 'html', 'jquery.ddMM.mm_ddYMap', '1.0.2');
 		//The Yandex.Maps library including
-		$output .= includeJsCss('http://api-maps.yandex.ru/2.0/?load=package.full&lang=ru-RU&onload=mm_ddYMap_init', 'html', 'api-maps.yandex.ru', '2.0');
+		$output = includeJsCss('http://api-maps.yandex.ru/2.1/?lang=ru_RU', 'html', 'api-maps.yandex.ru', '2.1');
+		//The jQuery.ddYMap library including
+		$output .= includeJsCss($modx->config['site_url'].'assets/plugins/managermanager/widgets/ddymap/jquery.ddYMap-1.3.1.min.js', 'html', 'jquery.ddYMap', '1.3.1');
+		//The main js file including
+		$output .= includeJsCss($modx->config['site_url'].'assets/plugins/managermanager/widgets/ddymap/jquery.ddMM.mm_ddYMap.js', 'html', 'jquery.ddMM.mm_ddYMap', '1.1');
 		
 		$e->output($output);
 	}else if ($e->name == 'OnDocFormRender'){
@@ -57,8 +59,8 @@ function mm_ddYMap($tvs, $roles = '', $templates = '', $w = 'auto', $h = '400', 
 '
 $j("#tv'.$usedTvs[$tv]['id'].'").mm_ddYMap({
 	hideField: '.intval($hideField).',
-	width: "'.$w.'",
-	height: "'.$h.'"
+	width: "'.$width.'",
+	height: "'.$height.'"
 });
 ';
 			}
--- assets/plugins/managermanager/widgets/ddymap/jquery.ddMM.mm_ddYMap.js
+++ assets/plugins/managermanager/widgets/ddymap/jquery.ddMM.mm_ddYMap.js
@@ -1,11 +1,13 @@
 /**
  * jQuery ddMM.mm_ddYMap Plugin
- * @version: 1.0.2 (2013-11-15)
+ * @version 1.1 (2015-05-07)
  * 
- * @uses jQuery 1.9.1
- * @uses $.ddMM 1.0
- *
- * @copyright 2013, DivanDesign
+ * @uses Yandex.Maps 2.1.
+ * @uses jQuery 1.10.2.
+ * @uses jQuery.ddMM 1.0.
+ * @uses jQuery.ddYMap 1.3.1.
+ * 
+ * @copyright 2015, DivanDesign
  * http://www.DivanDesign.biz
  */
 
@@ -20,87 +22,75 @@ $.ddMM.mm_ddYMap = {
 		//Высота контейнера с картой
 		height: 400
 	},
-	//Флаг сабмита поиска
-//	submitSerach: false,
-	//Массив id всех TV
-	tvs: new Array(),
-	//Загруженна ли карта
-	loaded: false,
 	//Инициализация карты
 	init: function(elem){
-		//Создаём карту
-		var map = new ymaps.Map('ddYMap' + elem.id, {
-	 			center: [elem.LngLat[0], elem.LngLat[1]],
-				zoom: 15,
-			}),
-			//Контрол поиска
-			serachControl = new ymaps.control.SearchControl({useMapBounds: true, noPlacemark: true, width: 400}),
-			//Создаём метку
-			marker = new ymaps.Placemark(
-				[elem.LngLat[0], elem.LngLat[1]],
-				{},
-				{draggable: true}
-			);
-	
-/*		//При сабмите поиска
-		serachControl.events.add('submit', function(){
-			//Укажем это
-			_this.submitSerach = true;
-		});*/
+		var $mapElement = $('#ddYMap' + elem.id);
 		
-		//При выборе результата поиска
-		serachControl.events.add('resultselect', function(event){
-			var coords = event.originalEvent.target.getResultsArray()[0].geometry.getCoordinates();
+		//После инициализации карты
+		$mapElement.on('ddAfterInit', function(){
+			//Объект карты
+			var map = $mapElement.data('ddYMap').map,
+				//Контрол поиска
+				serachControl = new ymaps.control.SearchControl({
+					options: {
+						useMapBounds: true,
+						noPlacemark: true,
+						maxWidth: 400
+					}
+				}),
+				//Метка.
+				//TODO: Это очень странно, но похоже, что «map.geoObjects.get(0)» возвращает «GeoObjectCollection» вместо «Placemark», потому приходится ещё раз делать «get(0)».
+				placemark = map.geoObjects.get(0).get(0);
 			
-			//Переместим куда надо маркер
-			marker.geometry.setCoordinates(coords);
+			//При выборе результата поиска
+			serachControl.events.add('resultselect', function(event){
+				var coords = event.originalEvent.target.getResultsArray()[0].geometry.getCoordinates();
+				
+				//Переместим куда надо маркер
+				placemark.geometry.setCoordinates(coords);
+				
+				//Запишем значение в оригинальное поле
+				elem.$elem.val(coords[0] + ',' + coords[1]);
+			});
 			
-			//Запишем значение в оригинальное поле
-			elem.$elem.val(coords[0] + ',' + coords[1]);
+			map.controls.add(serachControl);
+			
+			//При клике по карте меняем координаты метки
+			map.events.add('click', function(event){
+				var coords = event.get('coords');
+				
+				placemark.geometry.setCoordinates([coords[0], coords[1]]);
+				
+				elem.$elem.val(coords[0] + ',' + coords[1]);
+			});
+			
+			//Перетаскивание метки
+			placemark.events.add('dragend', function(event){
+				var coords = placemark.geometry.getCoordinates();
+				
+				elem.$elem.val(coords[0] + ',' + coords[1]);
+			});
+		}).ddYMap({
+			placemarks: elem.LngLat,
+			placemarkOptions: {draggable: true}
 		});
-		
-		//Добавляем контролы
-		map.controls
-			.add('zoomControl')
-			.add('typeSelector')
-			.add('scaleLine')
-			.add('mapTools')
-			.add(serachControl);
-		
-		//При клике по карте меняем координаты метки
-		map.events.add('click', function(event){
-			var coords = event.get('coordPosition');
-			
-			marker.geometry.setCoordinates([coords[0], coords[1]]);
-			elem.$elem.val(coords[0] + ',' + coords[1]);
-		});
-		
-		//Перетаскивание метки
-		marker.events.add('dragend', function(event){
-			var markerCoord = marker.geometry.getCoordinates();
-			
-			elem.$elem.val(markerCoord[0] + ',' + markerCoord[1]);
-		});
-		
-		//Добавляем метку на карту
-		map.geoObjects.add(marker);
 	}
 };
 
 /**
  * jQuery.fn.mm_ddYMap Plugin
- * @version 1.0 (2013-09-16)
+ * @version 1.0.2 (2015-05-07)
  * 
- * @description Делает карту.
+ * @desc Делает карту.
  * 
- * @uses $.ddMM.mm_ddYMap 1.0
+ * @uses $.ddMM.mm_ddYMap
  * 
- * Параметры передаются в виде plain object.
- * @param hideField {boolean} - Нужно ли скрывать оригинальное поле. Default: true.
- * @param width {integer; 'auto'} - Ширина контейнера с картой. Default: 'auto'.
- * @param height {integer} - Высота контейнера с картой. Default: 400.
+ * @param params {plain object} - Параметры передаются в виде plain object.
+ * @param params.hideField {boolean} - Нужно ли скрывать оригинальное поле. Default: true.
+ * @param params.width {integer; 'auto'} - Ширина контейнера с картой. Default: 'auto'.
+ * @param params.height {integer} - Высота контейнера с картой. Default: 400.
  * 
- * @copyright 2013, DivanDesign
+ * @copyright 2015, DivanDesign
  * http://www.DivanDesign.biz
  */
 $.fn.mm_ddYMap = function(params){
@@ -128,20 +118,20 @@ $.fn.mm_ddYMap = function(params){
 			//Запоминаем название поля
 			sectionName = $elemParent.find('.warning').text(),
 			//Контейнер для карты
-			$sectionContainer = $('<div class="sectionHeader">' + sectionName + '</div><div class="sectionBody tmplvars"><div id="ddYMap' + elem.id + '" style="width: ' + params.width + '; height: ' + params.height + 'px; position: relative; border: 1px solid #c3c3c3;"></div></div>'),
+			$sectionContainer = $('<div class="sectionHeader">' + sectionName + '</div><div class="sectionBody"><div id="ddYMap' + elem.id + '" style="width: ' + params.width + '; height: ' + params.height + 'px; position: relative; border: 1px solid #c3c3c3;"></div></div>'),
 			$YMap = $sectionContainer.find('#ddYMap' + elem.id);
-	
+		
 		//Добавляем контейнер
 		elem.$elem.parents('.tab-page:first').append($sectionContainer);
 		
 		//Скрываем родителя и разделитель
 		$elemParent.hide().prev('tr').hide();
-	
+		
 		//Если скрывать не надо, засовываем перед картой
 		if (!params.hideField){
 		 	elem.$elem.insertBefore($YMap);
 		}
-	
+		
 		//Если координаты не заданны, то задаём дефолт
 		if ($.trim(elem.LngLat) == ''){
 			elem.LngLat = '55.17725339420589,61.29035648102616';
@@ -150,41 +140,8 @@ $.fn.mm_ddYMap = function(params){
 		//Разбиваем координаты
 		elem.LngLat = elem.LngLat.split(',');
 		
-		//Если карта ещё не загруженна
-		if (!$.ddMM.mm_ddYMap.loaded){
-			//Просто запоминаем (инициализируется само при загрузке)
-			$.ddMM.mm_ddYMap.tvs.push(elem);
-		//Если же карта уже загружена
-		}else{
-			//Просто инициализируем
-			$.ddMM.mm_ddYMap.init(elem);
-		}
+		//Инициализируем
+		$.ddMM.mm_ddYMap.init(elem);
 	});
 };
-
-//On document.ready
-$(function(){
-/*	//Самбмит главной формы
-	$('#mutate').on('submit', function(){
-		//Если до этого был сабмит поиска
-		if ($.ddMM.mm_ddYMap.submitSerach){
-			//Сбросим защёлку
-			$.ddMM.mm_ddYMap.submitSerach = false;
-			//Выкидываем осечку
-			return false;
-		}
-	});*/
-	
-	//Глобальная инициализация карт (для солбэка от Яндекс.Карт)
-	window.mm_ddYMap_init = function(){
-		//Перебираем все
-		for (var i = $.ddMM.mm_ddYMap.tvs.length - 1; i >= 0; i--){
-			//Инициализируем карту для нужной TV
-			$.ddMM.mm_ddYMap.init($.ddMM.mm_ddYMap.tvs[i]);
-		}
-		
-		//Запоминаем, что первый раз карта уже инициализированна
-		$.ddMM.mm_ddYMap.loaded = true;
-	};
-});
 })(jQuery);
