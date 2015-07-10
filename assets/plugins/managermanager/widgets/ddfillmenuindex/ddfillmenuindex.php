7d9fbae40140c16042832263dd98604770ed54ed
Ilyas Ronef <ilyas@divandesign.biz>
2015-07-10 11:01:14 +0500

    * The “mm_ddFillMenuindex” widget has been updated to 1.1.

--- assets/plugins/managermanager/widgets/ddfillmenuindex/ddfillmenuindex.php
+++ assets/plugins/managermanager/widgets/ddfillmenuindex/ddfillmenuindex.php
@@ -1,17 +1,19 @@
 <?php
 /**
  * mm_ddFillMenuindex
- * @version 1.0 (2013-03-14)
- *
- * Widget sets the minimum free menuindex for a new documents. By default in MODx menuindex for a new documents equals just simply count of children, which is not always convenient.
- *
+ * @version 1.1 (2015-07-10)
+ * 
+ * @desc A widget for the ManagerManager plugin that was made to keep “menuindex” in order:
+ * 1. “menuindex” of a new document is set equal to a free minimal value within its parent (“menuindex”, by default, used to be the number of siblings which was not always preferred).
+ * 2. “menuindex” is incremented automatically by 1 on document duplicate.
+ * 
  * @uses ManagerManager plugin 0.5.
  * 
- * @param $parent {integer} - Id of parent document.
- *
- * @link http://code.divandesign.biz/modx/mm_ddfillmenuindex/1.0
- *
- * @copyright 2013, DivanDesign
+ * @param $parent {integer; ''} - Id of parent document. Default: '' (all parents).
+ * 
+ * @link http://code.divandesign.biz/modx/mm_ddfillmenuindex/1.1
+ * 
+ * @copyright 2015, DivanDesign
  * http://www.DivanDesign.biz
  */
 
@@ -30,23 +32,36 @@ function mm_ddFillMenuindex($parent = ''){
 		}
 		
 		//Получаем наименьший свободный menuindex у документов данного родителя. Кхэм, запрос писался глубокой ночью, так что за его оптимальность отвечать сложно ;-)
-		$freeMenuIndex = $modx->db->getValue("
-		SELECT min(`sc`.`menuindex`)
-		FROM `dd_site_content` AS `sc`
-			LEFT JOIN (
-				SELECT `sc1`.`menuindex`
-				FROM `dd_site_content` AS `sc1`, `dd_site_content` AS `sc2`
-				WHERE `sc1`.`menuindex` + 1 = `sc2`.`menuindex` AND `sc1`.`parent` = $pid AND `sc2`.`parent` = $pid
-			) AS `z`
-			ON `sc`.`menuindex` = `z`.`menuindex`
-		WHERE `z`.`menuindex` IS NULL AND `sc`.`parent` = $pid
-		");
+		$freeMenuIndex = $modx->db->getValue('
+			SELECT
+				min(`sc`.`menuindex`)
+			FROM `dd_site_content` AS `sc`
+				LEFT JOIN (
+					SELECT `sc1`.`menuindex`
+					FROM `dd_site_content` AS `sc1`, `dd_site_content` AS `sc2`
+					WHERE `sc1`.`menuindex` + 1 = `sc2`.`menuindex` AND `sc1`.`parent` = '.$pid.' AND `sc2`.`parent` = '.$pid.'
+				) AS `z`
+				ON `sc`.`menuindex` = `z`.`menuindex`
+			WHERE
+				`z`.`menuindex` IS NULL AND
+				`sc`.`parent` = '.$pid.'
+		');
 		
 		//Если такового нет (дочерних вообще нет). P.S.: Если он пуст, то переопределять нет смысла, там всё хорошо и так поставится.
 		if (!is_null($freeMenuIndex)){
 			//Задаём следующим
 			$content['menuindex'] = $freeMenuIndex + 1;
 		}
+	}else if ($e->name == 'OnDocDuplicate'){
+		//Инкрементируем menuindex при копировании
+		$modx->db->query('
+			UPDATE
+				'.ddTools::$tables['site_content'].'
+			SET
+				`menuindex` = `menuindex` + 1
+			WHERE
+				`id` = '.$e->params['new_id'].'
+		');
 	}
 }
 ?>
