e0392419d5f5f47263271fe2c5824b665012ca66
Ilyas Ronef <ilyas@divandesign.biz>
2016-07-01 20:15:33 +0500

    * The “mm_ddFillMenuindex” widget has been updated to 1.1.1.

--- assets/plugins/managermanager/widgets/ddfillmenuindex/ddfillmenuindex.php
+++ assets/plugins/managermanager/widgets/ddfillmenuindex/ddfillmenuindex.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * mm_ddFillMenuindex
- * @version 1.1 (2015-07-10)
+ * @version 1.1.1 (2016-07-01)
  * 
  * @desc A widget for the ManagerManager plugin that was made to keep “menuindex” in order:
  * 1. “menuindex” of a new document is set equal to a free minimal value within its parent (“menuindex”, by default, used to be the number of siblings which was not always preferred).
@@ -9,24 +9,24 @@
  * 
  * @uses ManagerManager plugin 0.5.
  * 
- * @param $parent {integer; ''} - Id of parent document. Default: '' (all parents).
+ * @param $parentId {integer|''} - Id of parent document. Default: '' (all parents).
  * 
- * @link http://code.divandesign.biz/modx/mm_ddfillmenuindex/1.1
+ * @link http://code.divandesign.biz/modx/mm_ddfillmenuindex/1.1.1
  * 
- * @copyright 2015, DivanDesign
- * http://www.DivanDesign.biz
+ * @copyright 2013–2016 DivanDesign {@link http://www.DivanDesign.biz }
  */
 
-function mm_ddFillMenuindex($parent = ''){
+function mm_ddFillMenuindex($parentId = ''){
 	global $modx, $content;
 	$e = &$modx->Event;
+	$siteContentTable = ddTools::$tables['site_content'];
 	
 	//Если у нас правильное событие и это создание документа
 	if ($e->name == 'OnDocFormPrerender' && $_REQUEST['a'] == 4){
-		$pid = intval($_REQUEST['pid']);
+		$documentParentId = intval($_REQUEST['pid']);
 		
 		//Если задан конкретный родитель, для которого должен работать виджет и он не совпадает с тем, что сейчас
-		if ($parent !== '' && $parent != $pid){
+		if ($parentId !== '' && $parentId != $documentParentId){
 			//Давай, до свидания!
 			return;
 		}
@@ -35,16 +35,16 @@ function mm_ddFillMenuindex($parent = ''){
 		$freeMenuIndex = $modx->db->getValue('
 			SELECT
 				min(`sc`.`menuindex`)
-			FROM `dd_site_content` AS `sc`
+			FROM '.$siteContentTable.' AS `sc`
 				LEFT JOIN (
 					SELECT `sc1`.`menuindex`
-					FROM `dd_site_content` AS `sc1`, `dd_site_content` AS `sc2`
-					WHERE `sc1`.`menuindex` + 1 = `sc2`.`menuindex` AND `sc1`.`parent` = '.$pid.' AND `sc2`.`parent` = '.$pid.'
+					FROM '.$siteContentTable.' AS `sc1`, '.$siteContentTable.' AS `sc2`
+					WHERE `sc1`.`menuindex` + 1 = `sc2`.`menuindex` AND `sc1`.`parent` = '.$documentParentId.' AND `sc2`.`parent` = '.$documentParentId.'
 				) AS `z`
 				ON `sc`.`menuindex` = `z`.`menuindex`
 			WHERE
 				`z`.`menuindex` IS NULL AND
-				`sc`.`parent` = '.$pid.'
+				`sc`.`parent` = '.$documentParentId.'
 		');
 		
 		//Если такового нет (дочерних вообще нет). P.S.: Если он пуст, то переопределять нет смысла, там всё хорошо и так поставится.
@@ -56,7 +56,7 @@ function mm_ddFillMenuindex($parent = ''){
 		//Инкрементируем menuindex при копировании
 		$modx->db->query('
 			UPDATE
-				'.ddTools::$tables['site_content'].'
+				'.$siteContentTable.'
 			SET
 				`menuindex` = `menuindex` + 1
 			WHERE
