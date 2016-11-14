c5f09b41cb5aa187d8c24c9c48aac8dc43c730bc
Ilyas Ronef <ilyas@divandesign.biz>
2016-11-15 02:06:30 +0500

    * The “mm_ddFillMenuindex” widget was updated to 1.2.

--- assets/plugins/managermanager/widgets/ddfillmenuindex/ddfillmenuindex.php
+++ assets/plugins/managermanager/widgets/ddfillmenuindex/ddfillmenuindex.php
@@ -1,32 +1,58 @@
 <?php
 /**
  * mm_ddFillMenuindex
- * @version 1.1.1 (2016-07-01)
+ * @version 1.2 (2016-11-15)
  * 
  * @desc A widget for the ManagerManager plugin that was made to keep “menuindex” in order:
  * 1. “menuindex” of a new document is set equal to a free minimal value within its parent (“menuindex”, by default, used to be the number of siblings which was not always preferred).
  * 2. “menuindex” is incremented automatically by 1 on document duplicate.
  * 
- * @uses ManagerManager plugin 0.5.
+ * @uses PHP >= 5.4.
+ * @uses MODXEvo.plugin.ManagerManager >= 0.7.
  * 
- * @param $parentId {integer|''} - Id of parent document. Default: '' (all parents).
+ * @param $params {array_associative|stdClass} — The object of params. @required
+ * @param $params['parentId'] {integer|''} — Id of parent document. Default: '' (all parents).
  * 
- * @link http://code.divandesign.biz/modx/mm_ddfillmenuindex/1.1.1
+ * @link http://code.divandesign.biz/modx/mm_ddfillmenuindex/1.2
  * 
  * @copyright 2013–2016 DivanDesign {@link http://www.DivanDesign.biz }
  */
 
-function mm_ddFillMenuindex($parentId = ''){
+function mm_ddFillMenuindex($params = []){
+	//For backward compatibility
+	if (
+		!is_array($params) &&
+		!is_object($params)
+	){
+		//Convert ordered list of params to named
+		$params = ddTools::orderedParamsToNamed([
+			'paramsList' => func_get_args(),
+			'compliance' => [
+				'parentId'
+			]
+		]);
+	}
+	
+	//Defaults
+	$params = (object) array_merge([
+		'parentId' => ''
+	], (array) $params);
+	
 	global $modx, $content;
 	$e = &$modx->Event;
-	$siteContentTable = ddTools::$tables['site_content'];
 	
 	//Если у нас правильное событие и это создание документа
-	if ($e->name == 'OnDocFormPrerender' && $_REQUEST['a'] == 4){
+	if (
+		$e->name == 'OnDocFormPrerender' &&
+		$_REQUEST['a'] == 4
+	){
 		$documentParentId = intval($_REQUEST['pid']);
 		
 		//Если задан конкретный родитель, для которого должен работать виджет и он не совпадает с тем, что сейчас
-		if ($parentId !== '' && $parentId != $documentParentId){
+		if (
+			$params->parentId !== '' &&
+			$params->parentId != $documentParentId
+		){
 			//Давай, до свидания!
 			return;
 		}
@@ -35,10 +61,10 @@ function mm_ddFillMenuindex($parentId = ''){
 		$freeMenuIndex = $modx->db->getValue('
 			SELECT
 				min(`sc`.`menuindex`)
-			FROM '.$siteContentTable.' AS `sc`
+			FROM '.ddTools::$tables['site_content'].' AS `sc`
 				LEFT JOIN (
 					SELECT `sc1`.`menuindex`
-					FROM '.$siteContentTable.' AS `sc1`, '.$siteContentTable.' AS `sc2`
+					FROM '.ddTools::$tables['site_content'].' AS `sc1`, '.ddTools::$tables['site_content'].' AS `sc2`
 					WHERE `sc1`.`menuindex` + 1 = `sc2`.`menuindex` AND `sc1`.`parent` = '.$documentParentId.' AND `sc2`.`parent` = '.$documentParentId.'
 				) AS `z`
 				ON `sc`.`menuindex` = `z`.`menuindex`
@@ -56,7 +82,7 @@ function mm_ddFillMenuindex($parentId = ''){
 		//Инкрементируем menuindex при копировании
 		$modx->db->query('
 			UPDATE
-				'.$siteContentTable.'
+				'.ddTools::$tables['site_content'].'
 			SET
 				`menuindex` = `menuindex` + 1
 			WHERE
