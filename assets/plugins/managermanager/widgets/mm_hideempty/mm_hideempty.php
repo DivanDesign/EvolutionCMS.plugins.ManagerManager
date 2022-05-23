9d606dcf62e073b0b8eb21a56dbcc35214b46c85
Ilyas Ronef <ilyas@DivanDesign.ru>
2022-05-23 14:57:18 +0300

    * Widgets → `mm_hideEmpty`: Updated from 0.2 to 1.0.

--- assets/plugins/managermanager/widgets/mm_hideempty/mm_hideempty.php
+++ assets/plugins/managermanager/widgets/mm_hideempty/mm_hideempty.php
@@ -1,27 +1,14 @@
 <?php
 /**
  * mm_hideEmpty
- * @version 0.2 (2016-11-05)
+ * @version 1.0 (2022-05-23)
  * 
- * @desc A widget for ManagerManager plugin that allows to hide all empty sections and tabs.
+ * @see README.md
  * 
- * @uses PHP >= 5.4.
- * @uses MODXEvo.plugin.ManagerManager >= 0.7.
- * 
- * @param $params {array_associative|stdClass} — The object of params. @required
- * @param $params['roles'] {string_commaSeparated} — Roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
- * @param $params['templates'] {string_commaSeparated} — Templates IDs for which the widget is applying (empty value means the widget is applying to all templates). Default: ''.
- * 
- * @event OnDocFormPrerender
- * @event OnDocFormRender
- * 
- * @author Sergey Davydov <webmaster@sdcollection.com>
- * @author DivanDesign <code@DivanDesign.biz>
- * 
+ * @link http://code.divandesign.biz/modx/mm_hideempty
  * @link https://github.com/MrSwed/MODXEvo.plugin.ManagerManager.mm_hideEmpty
- * @link http://code.divandesign.biz/modx/mm_hideempty/0.2
  * 
- * @copyright 2016
+ * @copyright 2016–2022
  */
 
 function mm_hideEmpty($params = []){
@@ -40,30 +27,46 @@ function mm_hideEmpty($params = []){
 		]);
 	}
 	
-	//Defaults
-	$params = (object) array_merge([
-		'roles' => '',
-		'templates' => ''
-	], (array) $params);
+	$params = \DDTools\ObjectTools::extend([
+		'objects' => [
+			//Defaults
+			(object) [
+				'roles' => '',
+				'templates' => ''
+			],
+			$params
+		]
+	]);
 	
-	if (!useThisRule($params->roles, $params->templates)){return;}
+	if (
+		!useThisRule(
+			$params->roles,
+			$params->templates
+		)
+	){
+		return;
+	}
 	
 	global $modx;
 	$e = &$modx->Event;
 	
-	// if the current page is being edited by someone in the list of roles, and uses a template in the list of templates
 	if ($e->name == 'OnDocFormPrerender'){
 		//The main js file including
-		$output = includeJsCss($modx->getConfig('site_url').'assets/plugins/managermanager/widgets/mm_hideempty/jQuery.ddMM.mm_hideEmpty.js', 'html', 'jQuery.ddMM.mm_hideEmpty', '1.0');
+		$output = includeJsCss(
+			$modx->getConfig('site_url') . 'assets/plugins/managermanager/widgets/mm_hideempty/jQuery.ddMM.mm_hideEmpty.js',
+			'html',
+			'jQuery.ddMM.mm_hideEmpty',
+			'1.2'
+		);
 		
 		$e->output($output);
 	}else if ($e->name == 'OnDocFormRender'){
-		$output = '//---------- mm_hideEmpty :: Begin -----'.PHP_EOL;
+		$output = '//---------- mm_hideEmpty :: Begin -----' . PHP_EOL;
 		
-		$output .= '$j.ddMM.mm_hideEmpty.hideEmptySections();'.PHP_EOL;
-		$output .= '$j.ddMM.mm_hideEmpty.hideEmptyTabs();'.PHP_EOL;
+		$output .= '$j.ddMM.mm_hideEmpty.hideEmptySections();' . PHP_EOL;
+		$output .= '$j.ddMM.mm_hideEmpty.hideEmptyTabs();' . PHP_EOL;
 		
-		$output .= '//---------- mm_hideEmpty :: End -----'.PHP_EOL;
+		$output .= '//---------- mm_hideEmpty :: End -----' . PHP_EOL;
 		
 		$e->output($output);
 	}
